7ee78758944df1bb33b11bfa0d3991e657ac057d
Dakshesh Bijawar <bdaksheshbijawar735@gmail.com>
2025-07-17 20:10:32 +0530

    Set up webpack, babel, and initial React structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Memory Card Game</h1>
+      <p>Project setup is complete!</p>
+    </div>
+  );
+}
+
+export default App;
